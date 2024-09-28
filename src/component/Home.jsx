90bf6327c97df53f222c7346cfdcd631b644ee14
vikash <infosotvikash@gmail.com>
2024-09-28 17:42:19 +0530

    added edit icon

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import BlogList from './BlogList';
 import { useNavigate } from 'react-router-dom';
-// Import Material UI icon for button
 import AddIcon from '@mui/icons-material/Add';
 
 function Home() {
@@ -12,13 +11,11 @@ function Home() {
     };
 
     return (
-        <div className='bg-slate-900 flex flex-col w-full min-h-screen justify-center items-center pt-10 px-6 lg:px-14'>
-            {/* Heading with more emphasis and responsive text size */}
+        <div className='bg-slate-900 flex flex-col w-full min-h-screen justify-center items-center pt-10 px-6 lg:px-8'>
             <h1 className="w-full lg:w-4/5 text-5xl lg:text-6xl text-teal-300 font-bold text-center mb-8 tracking-wide">
                 Blog's Collection
             </h1>
 
-            {/* Button styled with Material UI Icon */}
             <button 
                 onClick={handleAddBlog}
                 className='flex items-center justify-center gap-2 px-5 py-3 text-white rounded-lg bg-green-600 font-medium text-lg lg:text-xl hover:bg-green-700 transition-all duration-300 ease-in-out shadow-lg'
@@ -27,7 +24,6 @@ function Home() {
                 New Blog
             </button>
 
-            {/* BlogList Component */}
             <div className="w-full lg:w-11/12 mt-10">
                 <BlogList />
             </div>
